29625aec585fa873876cb3e9db5845bf340bd129
tsepton <tsepton@gmail.com>
2020-09-23 12:11:22 +0200

    Add css

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,23 +1,76 @@
 <script>
-	import Form from './Form.svelte';
-	
-	let response = [];
+  import Form from "./Form.svelte";
 
-	setInterval(() => {
-		response =
-			fetch('http://localhost:8000/messages')
-			.then(res => res.json());
-	}, 1000);
+  let response = [];
+  setInterval(() => {
+    response = fetch("http://localhost:8000/messages").then((res) =>
+      res.json()
+    );
+  }, 1000);
 </script>
+
+<style>
+  .container {
+    display: flex;
+	flex-wrap: wrap;
+	align-content: flex-start;
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (max-width: 540px) {
+    .message {
+      flex-basis: 100%;
+      background-color: #f2f2f2;
+	  width: 85%;
+	  height:fit-content;
+      border-radius: 3px;
+      margin: 0.7em;
+      padding: 1em;
+    }
+  }
+
+  @media (min-width: 540px) {
+    .message {
+      flex-basis: 25%;
+      background-color: #f2f2f2;
+	  width: 50%;
+	  height:max-content;
+      border-radius: 3px;
+      margin: 0.7em;
+      padding: 1em;
+    }
+  }
+
+  .message-text {
+    width: 100%;
+    display: block;
+  }
+
+  .message-author {
+    display: block;
+    font-weight: 100;
+    font-size: 1.5em;
+  }
+
+  .error {
+    font-weight: 100;
+  }
+</style>
 
 <Form />
 
-{#await response then messages}
-	{#each messages as message}
-		<p><b>{message.author}</b>: {message.text}</p>
-	{:else}
-		<p><i>Aucun message.</i></p>
-	{/each}
-{:catch error}
-	<p>{error}</p>
-{/await}
+<div class="container">
+  {#await response then messages}
+    {#each messages as message}
+      <div class="message">
+        <span class="message-text">{message.text}</span>
+        <span class="message-author"> - {message.author}</span>
+      </div>
+    {:else}
+      <div class="message"><span class="error">Aucun dixit.</span></div>
+    {/each}
+  {:catch error}
+    <div class="message"><span class="error">{error}</span></div>
+  {/await}
+</div>
