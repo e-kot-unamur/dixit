794f2e25312edd09592da1e6f52217f0b1c18708
Thibaut <tsepton@gmail.com>
2021-03-04 17:00:17 +0100

    Add date to messages

--- web-client/src/components/Messages.svelte
+++ web-client/src/components/Messages.svelte
@@ -21,7 +21,7 @@
     width: 85%;
     margin: 0.7rem;
     padding: 1rem;
-
+    position: relative;
     background-color: #f2f2f2;
     height: fit-content;
     border-radius: 3px;
@@ -31,12 +31,21 @@
   .message-text {
     width: 100%;
     display: block;
+    margin-bottom: 0.5rem;
   }
 
   .message-author {
     display: block;
     font-weight: 100;
     font-size: 1.5em;
+  }
+
+  .message-date {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    font-weight: 100;
+    font-size: 0.75em;
   }
 
   .error {
@@ -48,6 +57,9 @@
   {#await response then messages}
     {#each messages as message}
       <div class="message" key={message.text}>
+        <span class="message-date">
+          {(new Date(message.date).toLocaleString('en-GB', { timeZone: 'UTC' }))}
+        </span>
         <span class="message-text"><p />
           {message.text}</span>
         <span class="message-author"> - {message.author}</span>
